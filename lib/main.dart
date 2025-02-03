7a451642d1011576a597c74bdb333d99b18c0311
PigeonsHouse <bullbull1230@gmail.com>
2025-02-03 23:52:12 +0900

    update: ffmpegで選択した画像から動画を生成できるように

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'presentation/pages/main.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   const app = ProviderScope(child: MaterialApp(title: 'Voit', home: MainApp()));
   runApp(app);
 }
--- lib/presentation/pages/edit.dart
+++ lib/presentation/pages/edit.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
+import 'package:ffmpeg_kit_flutter_min_gpl/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_min_gpl/return_code.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:voit/presentation/states/main/edit_data.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
 
 enum RootMenuState {
   cameraRoll,
@@ -11,13 +17,33 @@ enum RootMenuState {
 
 final imagePicker = ImagePicker();
 
-class Edit extends ConsumerWidget {
+class Edit extends ConsumerStatefulWidget {
   const Edit({super.key});
 
+  @override
+  EditState createState() => EditState();
+}
+
+class EditState extends ConsumerState<Edit> {
+  String? _imagePath;
+  String? _videoPath;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   Future<void> pickImage() async {
-    XFile? file = await imagePicker.pickImage(source: ImageSource.camera);
+    final file = await imagePicker.pickImage(source: ImageSource.gallery);
     if (file != null) {
-      print(file.path);
+      final dir = await getApplicationDocumentsDirectory();
+      final fileName = path.basename(file.path);
+      final savePath = path.join(dir.path, fileName);
+      await File(file.path).copy(savePath);
+      setState(() {
+        _imagePath = savePath;
+      });
+      print(savePath);
     }
   }
 
@@ -28,8 +54,44 @@ class Edit extends ConsumerWidget {
     }
   }
 
+  Future<void> startEncode() async {
+    if (_imagePath == null) return;
+
+    Directory? externalDir;
+    if (Platform.isAndroid) {
+      externalDir = await getExternalStorageDirectory();
+    } else if (Platform.isIOS) {
+      externalDir = await getApplicationDocumentsDirectory();
+    }
+
+    if (externalDir == null) return;
+
+    const videoFileName = 'video_from_image.mp4';
+    final videoOutputPath = path.join(externalDir.path, videoFileName);
+
+    final ffmpegCommand =
+        '-loop 1 -i $_imagePath -c:v libx264 -t 3 -pix_fmt yuv420p -vf scale=trunc(iw/2)*2:trunc(ih/2)*2 "$videoOutputPath"';
+    await FFmpegKit.execute(ffmpegCommand).then((session) async {
+      final returnCode = await session.getReturnCode();
+      final logs = await session.getAllLogs();
+      print('FFmpeg returnCode: $returnCode');
+      for (final log in logs) {
+        print('FFmpeg log: ${log.getMessage()}');
+      }
+      if (ReturnCode.isSuccess(returnCode)) {
+        setState(() {
+          _videoPath = videoOutputPath;
+        });
+        print(videoOutputPath);
+      } else {
+        final failStackTrace = await session.getFailStackTrace();
+        print('FFmpeg execution failed: $failStackTrace');
+      }
+    });
+  }
+  
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final targetEditData = ref.watch(editDataNotifierProvider);
 
     return Scaffold(
@@ -37,10 +99,7 @@ class Edit extends ConsumerWidget {
         title: const Text('EDIT'),
         backgroundColor: Colors.greenAccent,
         actions: [
-          IconButton(onPressed: () => {
-
-
-          }, icon: const Icon(Icons.file_upload))
+          IconButton(onPressed: startEncode, icon: const Icon(Icons.file_upload))
         ],
       ),
       body: SafeArea(
