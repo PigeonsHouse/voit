97c8343d19ee1d661d7554221cbb7506276d1b36
PigeonsHouse <bullbull1230@gmail.com>
2025-06-16 14:00:37 +0900

    add: editの大枠の作成

--- src/presentation/screens/edit/edit.tsx
+++ src/presentation/screens/edit/edit.tsx
@@ -1,12 +1,29 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Edit: React.FC = () => {
   return (
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <Text>edit</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 10,
+          justifyContent: "center",
+          backgroundColor: "lightgray",
+          alignItems: "center",
+        }}
+      >
+        <Text>preview</Text>
+      </View>
+      <View
+        style={{
+          flex: 8,
+          justifyContent: "center",
+          backgroundColor: "gray",
+          alignItems: "center",
+        }}
+      >
+        <Text>timeline</Text>
+      </View>
     </SafeAreaView>
   );
 };
