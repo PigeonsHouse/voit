3e60e3d659e455b87aca27b9804a5cf9a10b75fe
PigeonsHouse <bullbull1230@gmail.com>
2025-06-20 22:32:58 +0900

    update: editの雛形の微修正

--- src/presentation/screens/edit/edit.tsx
+++ src/presentation/screens/edit/edit.tsx
@@ -3,27 +3,39 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export const Edit: React.FC = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View
-        style={{
-          flex: 10,
-          justifyContent: "center",
-          backgroundColor: "lightgray",
-          alignItems: "center",
-        }}
-      >
-        <Text>preview</Text>
-      </View>
-      <View
-        style={{
-          flex: 8,
-          justifyContent: "center",
-          backgroundColor: "gray",
-          alignItems: "center",
-        }}
-      >
-        <Text>timeline</Text>
-      </View>
-    </SafeAreaView>
+    <View style={{ flex: 1, backgroundColor: "black" }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            flex: 6,
+            justifyContent: "center",
+            backgroundColor: "lightgray",
+            alignItems: "center",
+          }}
+        >
+          <Text>preview</Text>
+        </View>
+        <View
+          style={{
+            flex: 4,
+            justifyContent: "center",
+            backgroundColor: "gray",
+            alignItems: "center",
+          }}
+        >
+          <Text>timeline</Text>
+        </View>
+        <View
+          style={{
+            height: 64,
+            justifyContent: "center",
+            backgroundColor: "white",
+            alignItems: "center",
+          }}
+        >
+          <Text>bottom menu</Text>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
